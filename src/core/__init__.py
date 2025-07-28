db9a42b2d9db3802e8eddb2a1079417c5b199d69
Danwuoo <wurrenren1006@gmail.com>
2025-07-28 09:26:03 +0800

    Add DAG engine skeleton

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,1 +1,23 @@
-# core module
+"""Core module exports."""
+
+from .agent import run
+from .dag_engine import (
+    DAGFlowBuilder,
+    DAGFlowRunner,
+    NodeWrapper,
+    ContextInjector,
+    ReplayManager,
+    build_trace_id,
+    register_node,
+)
+
+__all__ = [
+    "run",
+    "DAGFlowBuilder",
+    "DAGFlowRunner",
+    "NodeWrapper",
+    "ContextInjector",
+    "ReplayManager",
+    "build_trace_id",
+    "register_node",
+]
